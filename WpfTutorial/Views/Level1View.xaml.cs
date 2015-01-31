3065d52b726d73e5ee5510267e61e45dc547872a
brunolm7 <brunolm7@gmail.com>
2015-01-31 14:49:42 -0200

    Level name metadata

--- WpfTutorial/Views/Level1View.xaml.cs
+++ WpfTutorial/Views/Level1View.xaml.cs
@@ -5,7 +5,7 @@ using WpfTutorial.Extenders;
 
 namespace WpfTutorial.Views
 {
-    [MetadataExport(typeof(Level1View), typeof(ILevelControl), "Level 1 - Doom")]
+    [MetadataExport(typeof(Level1View), typeof(ILevelControl), "Level 1 - Exception on pay")]
     /// <summary>
     /// Interaction logic for Level1View.xaml
     /// </summary>
