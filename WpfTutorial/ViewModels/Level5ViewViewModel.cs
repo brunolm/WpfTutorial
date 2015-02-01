77846e0a6c285a06cbf39fe320c3f4a4bd8dad9d
brunolm7 <brunolm7@gmail.com>
2015-02-01 18:22:47 -0200

    Level 5

---
+++ WpfTutorial/ViewModels/Level5ViewViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using WpfTutorial.Extenders;
+
+namespace WpfTutorial.ViewModels
+{
+    [ExportViewModel(typeof(Level5ViewViewModel))]
+    public class Level5ViewViewModel : BaseLevelViewModel
+    {
+        private ObservableCollection<Game> games;
+        public ObservableCollection<Game> Games
+        {
+            get { return games; }
+            set
+            {
+                games = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public RelayCommand ShowInfoCommand { get; private set; }
+
+        public class Game
+        {
+            public string Name { get; set; }
+
+            public ObservableCollection<Character> Characters { get; set; }
+        }
+
+        public class Character
+        {
+            public string Name { get; set; }
+        }
+
+        public Level5ViewViewModel()
+        {
+            ShowInfoCommand = new RelayCommand(ShowInfoExecute);
+
+            Games = new ObservableCollection<Game>(new List<Game>
+            {
+                new Game 
+                { 
+                    Name = "Breath of Fire IV",
+                    Characters = new ObservableCollection<Character>(new List<Character> 
+                    {
+                        new Character { Name = "Ryu" },
+                        new Character { Name = "Fou-lu" },
+                    }),
+                },
+                new Game 
+                { 
+                    Name = "Skyrim",
+                    Characters = new ObservableCollection<Character>(new List<Character> 
+                    {
+                        new Character { Name = "Last Dragonborn" },
+                        new Character { Name = "Alduin" },
+                    }),
+                },
+            });
+        }
+
+        public override void OnLoaded(object sender)
+        {
+            #region Level info
+
+            Description = @"Level 5 - Can't get it right
+Expected result
+===============
+When I right click a game and click Show Info I should be able to see the game name properly.
+
+Actual result
+===============
+When I right click a game and show info I get an exception.
+
+Steps to reproduce:
+1. Select a game from the TreeView
+2. Right click
+3. Click Show Info
+
+Note: it seems to be woring fine for Characters
+";
+
+            #endregion
+        }
+
+        private void ShowInfoExecute(object obj)
+        {
+            var entity = (Character)obj;
+
+            MessageBox.Show(entity.Name, "Selected");
+        }
+    }
+}
---
+++ WpfTutorial/Views/Level5View.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using WpfTutorial.Extenders;
+
+namespace WpfTutorial.Views
+{
+    [MetadataExport(typeof(Level5View), typeof(ILevelControl), "Level 5 - Can't get it right")]
+    /// <summary>
+    /// Interaction logic for Level3View.xaml
+    /// </summary>
+    public partial class Level5View : UserControl, ILevelControl
+    {
+        public Level5View()
+        {
+            InitializeComponent();
+            this.ResolveDependencies();
+        }
+    }
+}
