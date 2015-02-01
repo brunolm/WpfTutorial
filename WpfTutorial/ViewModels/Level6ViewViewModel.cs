9e03fc9f504376d80cbad88d4f92467fb6a037af
brunolm7 <brunolm7@gmail.com>
2015-02-01 18:40:02 -0200

    Level 6

---
+++ WpfTutorial/ViewModels/Level6ViewViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using WpfTutorial.Extenders;
+
+namespace WpfTutorial.ViewModels
+{
+    [ExportViewModel(typeof(Level6ViewViewModel))]
+    public class Level6ViewViewModel : BaseLevelViewModel
+    {
+        public string SomeInput { get; set; }
+
+        public RelayCommand ChangeTextCommand { get; private set; }
+
+        public Level6ViewViewModel()
+        {
+            ChangeTextCommand = new RelayCommand(ChangeTextExecute);
+
+            SomeInput = "Initial text";
+            this.RaisePropertyChanged("SomeInput");
+        }
+
+        public override void OnLoaded(object sender)
+        {
+            #region Level info
+
+            Description = @"Level 6 - Can't change
+Expected result
+===============
+When I click the Change button the text should change.
+
+Actual result
+===============
+When I click the Change button the text does not change.
+
+Steps to reproduce:
+1. Click the Change button
+";
+
+            #endregion
+        }
+
+        private void ChangeTextExecute(object obj)
+        {
+            SomeInput = "Changed";
+        }
+    }
+}
---
+++ WpfTutorial/Views/Level6View.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using WpfTutorial.Extenders;
+
+namespace WpfTutorial.Views
+{
+    [MetadataExport(typeof(Level6View), typeof(ILevelControl), "Level 6 - Can't change")]
+    /// <summary>
+    /// Interaction logic for Level3View.xaml
+    /// </summary>
+    public partial class Level6View : UserControl, ILevelControl
+    {
+        public Level6View()
+        {
+            InitializeComponent();
+            this.ResolveDependencies();
+        }
+    }
+}
