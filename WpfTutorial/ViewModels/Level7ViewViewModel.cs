55c87ef2299318711ff959d6adddc0bf77ef857e
brunolm7 <brunolm7@gmail.com>
2015-02-01 19:35:58 -0200

    Level 7

---
+++ WpfTutorial/ViewModels/Level7ViewViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using WpfTutorial.Extenders;
+
+namespace WpfTutorial.ViewModels
+{
+    [ExportViewModel(typeof(Level7ViewViewModel))]
+    public class Level7ViewViewModel : BaseLevelViewModel
+    {
+        public Level7ViewViewModel()
+        {
+        }
+
+        public override void OnLoaded(object sender)
+        {
+            #region Level info
+
+            Description = @"Level 7 - I wish...
+
+As a user I want to be able to create a wishlist so I can remember what to buy next.
+
+Acceptance Criteria
+- I can enter a name for the item I want to add to my wishlist
+- I can click a button to add the item to the wishlist
+- I can click a button to remove the selected item from the wishlist
+";
+
+            #endregion
+        }
+    }
+}
---
+++ WpfTutorial/Views/Level7View.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows.Controls;
+using WpfTutorial.Extenders;
+
+namespace WpfTutorial.Views
+{
+    [MetadataExport(typeof(Level7View), typeof(ILevelControl), "Level 7 - I wish...")]
+    /// <summary>
+    /// Interaction logic for Level3View.xaml
+    /// </summary>
+    public partial class Level7View : UserControl, ILevelControl
+    {
+        public Level7View()
+        {
+            InitializeComponent();
+            this.ResolveDependencies();
+        }
+    }
+}
